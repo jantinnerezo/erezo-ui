c51dce5ec94bee67ff3042f7559959d1acf051ef
Jantinn Erezo <erezojantinn@gmail.com>
2020-11-29 15:44:46 +0800

    UI changes

--- resources/views/components/table.blade.php
+++ resources/views/components/table.blade.php
@@ -1,12 +1,25 @@
-<div class="align-middle min-w-full overflow-x-auto shadow overflow-hidden sm:rounded-lg">
-    <table class="min-w-full divide-y divide-cool-gray-200">
+@props(['options' => []])
+<div
+    x-data='{
+        options: {
+            valueNames:  @json($options)
+        }
+    }'
+    x-init="
+        new List($refs.table, this.options);
+    "
+    class="align-middle min-w-full overflow-x-auto shadow overflow-hidden sm:rounded-lg"
+    wire:ignore
+    x-ref="table"
+>
+    <table class="min-w-full divide-y divide-cool-gray-200" >
         <thead>
             <tr>
                 {{ $head }}
             </tr>
         </thead>
 
-        <tbody class="bg-white divide-y divide-cool-gray-200">
+        <tbody class="bg-white divide-y divide-cool-gray-200 list">
             {{ $body }}
         </tbody>
     </table>
--- resources/views/components/table/heading.blade.php
+++ resources/views/components/table/heading.blade.php
@@ -4,7 +4,7 @@
 ])
 
 <th
-    {{ $attributes->merge(['class' => 'px-6 py-3 bg-cool-gray-50'])->only('class') }}
+    {{ $attributes->merge(['class' => 'px-6 py-3 bg-cool-gray-50']) }}
 >
     @unless($sortable)
         <span class="text-left text-xs leading-4 font-medium text-cool-gray-500 uppercase tracking-wider">
