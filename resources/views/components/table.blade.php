7707cfaab6ba2418f52d792fb727fa68a86cefc3
Jantinn Erezo <erezojantinn@gmail.com>
2020-12-05 16:49:24 +0800

    Fixed table ui

--- resources/views/components/table.blade.php
+++ resources/views/components/table.blade.php
@@ -1,11 +1,15 @@
-<table class="min-w-full divide-y divide-cool-gray-200" >
-    <thead>
-        <tr>
-            {{ $head }}
-        </tr>
-    </thead>
+<div
+    class="align-middle min-w-full overflow-x-auto shadow overflow-hidden sm:rounded-lg""
+>
+    <table class="min-w-full divide-y divide-cool-gray-200" >
+        <thead>
+            <tr>
+                {{ $head }}
+            </tr>
+        </thead>
 
-    <tbody class="bg-white divide-y divide-cool-gray-200 list">
-        {{ $body }}
-    </tbody>
-</table>
+        <tbody class="bg-white divide-y divide-cool-gray-200 list">
+            {{ $body }}
+        </tbody>
+    </table>
+</div>
