c4c0b4b5b42c4d52f11e938d89698fb7d0a87ebc
Jantinn Erezo <erezojantinn@gmail.com>
2020-12-05 16:33:09 +0800

    Fixed table ui

--- resources/views/components/table.blade.php
+++ resources/views/components/table.blade.php
@@ -1,26 +1,11 @@
-@props(['options' => []])
-<div
-    x-data='{
-        options: {
-            valueNames:  @json($options)
-        }
-    }'
-    x-init="
-        new List($refs.table, this.options);
-    "
-    class="align-middle min-w-full overflow-x-auto shadow overflow-hidden sm:rounded-lg"
-    wire:ignore
-    x-ref="table"
->
-    <table class="min-w-full divide-y divide-cool-gray-200" >
-        <thead>
-            <tr>
-                {{ $head }}
-            </tr>
-        </thead>
+<table class="min-w-full divide-y divide-cool-gray-200" >
+    <thead>
+        <tr>
+            {{ $head }}
+        </tr>
+    </thead>
 
-        <tbody class="bg-white divide-y divide-cool-gray-200 list">
-            {{ $body }}
-        </tbody>
-    </table>
-</div>
+    <tbody class="bg-white divide-y divide-cool-gray-200 list">
+        {{ $body }}
+    </tbody>
+</table>
