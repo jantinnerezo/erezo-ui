06140b0a81907d8601f0e625872db27f2f032ff6
Jantinn Erezo <erezojantinn@gmail.com>
2021-01-25 22:51:51 +0800

    Updated select

--- resources/views/components/select.blade.php
+++ resources/views/components/select.blade.php
@@ -1,15 +1,19 @@
 <div
     x-data="Select.data({
-        model: '{!! $attributes['wire:model'] !!}',
-        placeholder: '{!! $attributes['placeholder'] !!}'
+        model: '{!! $attributes['wire:model'] ?? $attributes['name'] !!}',
+        placeholder: '{!! $attributes['placeholder'] !!}',
+        native: {!! $attributes->has('native') ? 1 : 0 !!}
     })"
     x-init="onInit()"
     wire:ignore
 >
     <select
-        x-ref="{{ $attributes['wire:model'] }}"
+        x-ref="{{ $attributes['wire:model'] ?? $attributes['name'] }}"
         {{ $attributes->except('wire:model') }}
     >
         {{ $slot }}
     </select>
+    @if ($attributes->has('native') && $attributes->has('multiple'))
+        <input type="hidden" readonly x-ref="selected" name="{{ $attributes['name'] }}">
+    @endif
 </div>
